f597bdbbf946f6936f4cb2e55c3b1e8bae1ecfd8
Daniel Jajliardo <TNTWZRD.minecraft@gmail.com>
2015-07-30 18:37:11 -0400

    added more options

--- ComputerChat/chat.cpp
+++ ComputerChat/chat.cpp
@@ -5,6 +5,7 @@ chat::chat()
 	enum programState
 	{
 		STARTUP,
+		HOSTING_MODE,
 		MAINMENU,
 		CHATSCREEN,
 		LOGOUT
@@ -15,8 +16,28 @@ chat::chat()
 	while (pState == programState::STARTUP) // Startup Code
 	{
 		cout << "Loading TheTechSphere Computer Chat Application..." << endl << "Software Version 00.01.01" << endl;
-
-		char i = _getch();
+		bool modeSelection = 0;
+		while(!modeSelection)
+		{
+			cout << "Please Select A Mode: (1)HOST (2)CLIENT" << endl;
+			char i = _getch();
+			switch (i)
+			{
+			case '1':
+				modeselection = 1;
+				pState = programState::HOSTING_MODE;
+				break;
+			case '2':
+				modeselection = 1;
+				pState = programState::MAINMENU;
+				break;
+			defualt:
+				cout << "Please Enter Either (1) Or (2)......" << endl;
+				breack;
+			}
+		}
+		
+		
 		pState = programState::LOGOUT;
 	}
 
