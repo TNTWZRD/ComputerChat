ccb7811ed4ac6fe544dbf116793a9c72efd005cc
Corbin Matschull <c.matschull@icloud.com>
2015-08-01 23:19:58 -0600

    Update clientSocket.h

--- ComputerChat/clientSocket.h
+++ ComputerChat/clientSocket.h
@@ -1,11 +1,48 @@
 #pragma once
 
+#ifndef CSOCK_H
+#define CSOCK_H
+
+#include <cstdint>
 #include <winsock.h>
+
+#define MIN_PIPED_OBJECTS 1
+
+enum PORT_MESSAGE
+{
+    SOCK_CANNOT_LISTEN,
+    SOCK_WAITING,
+    SOCK_DEAD,
+    SOCK_TIMEOUT,
+    SOCK_CONNECTED,
+    SOCK_PIPE_ACTIVE,
+};
+
+// ---------------------
+// Client Port Defaults
+// ---------------------
+
+unsigned int __defaultPort = 2202;
+
+// ---------------------
+// ! Client Port defaults !
+// ---------------------
 
 class clientSocket
 {
 public:
-	bool ConnectToHost(int PortNo, char* IPAddress);
-	void CloseConnection();
+    /**
+     * \param _portno - Default client port
+     * \param clientUID - Default client name to pass to the server
+     */
+    clientSocket(int _portno, string clientUID);
+
+    bool ConnectToHost(int PortNo, char* IPAddress);
+
+    void CloseConnection();
+
+    // Deconstructor
+    virtual ~clientSocket();
 };
 
+#endif // defined(CSOCK_H)
