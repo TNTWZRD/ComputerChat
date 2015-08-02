7f53bfc7f7ff2b129ff7586c9ded3a40947fcd2e
Polyg0n <c.matschull@icloud.com>
2015-08-02 17:36:43 -0600

    autogit: Pushing for staging to VS2015

--- ComputerChat/Handshake.h
+++ ComputerChat/Handshake.h
@@ -9,13 +9,17 @@
 #include "ClientMode.h"
 #include "HostMode.h"
 
+#include "typekey.h"
+
 class Handshake : protected ClientMode
 {
 
     public:
         Handshake();
 
-        DWORD32 commitHandshake(string _serverPassword);
+        DWORD32 commitHandshake(string _sPass) : _serverPassword(_sPass);
+
+        long traceShake(std::vector<long> tracePath) __KEY_EXPORT;
 };
 
 #endif // defined(HANDSHAKE_H)
