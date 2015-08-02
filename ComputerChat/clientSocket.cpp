2364127c5db3f0faedf20659cae6f33a144bd9cb
Daniel Jajliardo <TNTWZRD.minecraft@gmail.com>
2015-08-01 21:27:35 -0400

    Added cleintSocket to files

---
+++ ComputerChat/clientSocket.cpp
@@ -0,0 +1,49 @@
+#include "clientSocket.h"
+
+SOCKET s;
+
+bool clientSocket::ConnectToHost(int PortNo, char* IPAddress)
+{
+	OutputDebugString("[CleintSocket].[ConnectToHost]: Beginning Connection Sequence.\n");
+	WSADATA wsadata;
+
+	int error = WSAStartup(0x0202, &wsadata);
+
+	if (error)
+		return false;
+
+	if (wsadata.wVersion != 0x0202)
+	{
+		WSACleanup();
+		return false;
+	}
+
+	SOCKADDR_IN target;
+
+	target.sin_family = AF_INET;
+	target.sin_port = htons(PortNo);
+	target.sin_addr.s_addr = inet_addr(IPAddress);
+
+	s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (s == INVALID_SOCKET)
+		return false;
+
+	if (connect(s, (SOCKADDR *)&target, sizeof(target)) == SOCKET_ERROR)
+	{
+		OutputDebugString("[CleintSocket].[ConnectToHost]: Connection Refused.\n");
+		return false;
+	}
+	else
+	{
+		OutputDebugString("[CleintSocket].[ConnectToHost]: Connection Success.\n");
+		return true;
+	}
+}
+
+void clientSocket::CloseConnection()
+{
+	if (s)
+		closesocket(s);
+
+	WSACleanup();
+}
